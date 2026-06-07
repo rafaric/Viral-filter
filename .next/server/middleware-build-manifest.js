6c310f0f728a7aad76a78090bf5df5e3c8eec3dd
Rafael Strongoli <rafaelstrongoli@gmail.com>
2026-06-07 17:46:02 -0300

    fix: CSS and build configuration fixes

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/0cz1d0mv5g_q7.js"
+  ],
+  "lowPriorityFiles": [
+    "static/isXKQuHGcQSvpNIgOwKXN/_buildManifest.js",
+    "static/isXKQuHGcQSvpNIgOwKXN/_ssgManifest.js",
+    "static/isXKQuHGcQSvpNIgOwKXN/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/3z5q_p4msz2ha.js",
+    "static/chunks/0atut6a2uuyid.js",
+    "static/chunks/3n7dm2ojtyzwn.js",
+    "static/chunks/turbopack-1gvcuqh398wmk.js"
+  ]
+};
